9b655415a5277a029745335b2890066828b80bff
Saravanaanandh <saravanaanandh2006@gmail.com>
2026-03-02 11:54:44 +0530

    event organizer

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -16,7 +16,7 @@ const AboutPage = () => {
       highlight: true
     },
     associateProfessors: [
-      { name: "Dr. R. Bhavani", role: "Associate Professor (Symposium Coordinator)", highlight: true },
+      { name: "Dr. R. Bhavani", role: "Associate Professor (Event Organizer)" },
       { name: "Dr. S. Nagarajan", role: "Associate Professor (CAS)" }
     ],
     assistantProfessors: [
